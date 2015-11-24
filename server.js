e4880ca37587c0c2c2793714f873c3628aca5bdb
NelsonRi21 <NelsonRi21@gmail.com>
2015-11-24 08:55:24 -0500

    Added todos page

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Call Mom',
+	completed: false,
+	type: 'string'
+}, {
+	id: 2,
+	description: 'Go grocery shopping',
+	completed: false,
+	type: 'string'
+}];
 
 app.get('/', function (req, res) {
-	res.send('Todo API Root')
+	res.send('Todo API Root');
+});
+
+app.get('/todos', function (req, res) {
+	res.send('Todo page is live!');
 });
 
 app.listen(PORT, function () {
