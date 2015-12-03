07726da268f1473d4e41874e7b8cf6b1f3183704
NelsonRi <NelsonRi21@gmail.com>
2015-12-03 08:35:34 -0500

    Added PUT route

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId})
 
-	/* 
+	/* Refactored to _.js
 	var matchedTodo;
 
 	todos.forEach(function (todo) {
@@ -70,6 +70,40 @@ app.delete('/todos/:id', function (req, res) {
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
+});
+
+//PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} else {
+		// Never provided attribute, no problem here.
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length === 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	} else {
+		// Never provided attribute, no problem here.
+	}
+
+	// Extend gives the ability to copy the objects from a SOURCE to a DESTINATION 
+	// _.extend(destination, *sources); (objects in Javascript are passed by reference, not value)
+	_.extend(matchedTodo, validAttributes);
+
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
