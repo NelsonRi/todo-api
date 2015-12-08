9d14177a3a4f76b289c94a26ce003d709277ed8e
NelsonRi <NelsonRi21@gmail.com>
2015-12-08 08:26:14 -0500

    Added sequelize demo

---
+++ playground/variable-playground.js
@@ -0,0 +1,13 @@
+// Array Example
+var grades = [15, 88];
+
+function addGrades (grades) {
+	grades.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
+
+// node debug FILENAME
+// cont, repl, kill, quit
